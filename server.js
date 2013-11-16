1a6262b965c8b344764b6cbf35780dfbc4763f3d
Julian Alexander Murillo <julixrevolutions@gmail.com>
2013-11-16 00:41:31 -0500

    New changes! now it will work

--- server.js
+++ server.js
@@ -1,16 +1,45 @@
-var express = require('express'), app = express.createServer();
+
+console.log("(+) Load requirments...");
+var express = require('express'), app = express();
+
+console.log("(+) Create server...");
+//var server = require('http').createServer(app);
+
+var http = require('http');
+
+//var io = require('socket.io').listen(server);
+
+console.log("(+) Require jade...");
 var jade = require('jade');
-var io = require('socket.io').listen(app);
+
+console.log("(+) Requirments done.");
+
+console.log("(+) Set paths")
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set("view options", { layout: false });
 app.configure(function() {
+	app.set('port', process.env.PORT || 3000);
 	app.use(express.static(__dirname + '/public'));
 });
+
+
 app.get('/', function(req, res){
   res.render('home.jade');
 });
-app.listen(3000);
+
+//port = 3000;
+//app.listen(port);
+
+//console.log("(+) Listening at port: " + port);
+
+var server = http.createServer(app).listen(app.get('port'), function() {
+    console.log("Express server listening on port " + app.get('port'));
+});
+
+console.log("(+) Listen socket...");
+var io = require('socket.io').listen(server);
+
 io.sockets.on('connection', function (socket) {
 	socket.on('setPseudo', function (data) {
 		socket.set('pseudo', data);
